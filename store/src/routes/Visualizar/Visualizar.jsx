a737ac52f5950db3f9ef5e4f00985216a342845a
santanaIgot <santanaigoor01@gmail.com>
2023-09-13 10:50:30 -0300

    Visualizar.jsx

---
+++ store/src/routes/Visualizar/Visualizar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Visualizar() {
+    return(
+        <>
+
+        <main>
+            <h1>Aparelho</h1>
+        </main>
+
+        </>
+    )
+}
