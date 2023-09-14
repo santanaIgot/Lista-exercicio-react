86de7e6b8392edc855aaa2ace4257d44e096d069
santanaIgot <santanaigoor01@gmail.com>
2023-09-13 21:07:32 -0300

    Aparelhos.jsx estruturado

--- store/src/routes/Aparelhos/Aparelhos.jsx
+++ store/src/routes/Aparelhos/Aparelhos.jsx
@@ -1,12 +1,18 @@
-import React from "react";
-
+import { Link } from "react-router-dom";
+import './Aparelhos.css'
+import { Outlet } from "react-router-dom";
 
 export default function Aparelhos() {
   return (
+
+   
     <>
-      <main>
+      <main className="o">
         <h1>oi mundo</h1>
       </main>
+
+      
+      
     </>
-  );
+  )
 }
